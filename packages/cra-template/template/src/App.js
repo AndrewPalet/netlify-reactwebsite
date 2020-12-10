a421765a9ced2cb50451ea4fcbb3e88c27ef85b5
“AndrewPalet” <palet5796@gmail.com>
2020-12-10 14:07:47 -0600

    connected netlify

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is an example of Hosting a website!
+          Andrew Palet's Continuous Integration
         </a>
       </header>
     </div>
